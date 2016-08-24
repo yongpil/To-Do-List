ef1d3c68e57b649334345cb39fcc528b8f6f240d
yongpil <myp7166@naver.com>
2016-08-24 20:15:13 +0900

    post 요청처리 및 파이썬 변수 템플릿에 출력

--- lists/tests.py
+++ lists/tests.py
@@ -14,6 +14,30 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('home.html',request=request)
         self.assertEqual(response.content.decode(), expected_html)
+    def test_home_page_can_sace_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '신규 작업 아이템'
+
+        response = home_page(request)
+
+        self.assertIn('신규 작업 아이템', response.content.decode())
+        expected_html = render_to_string(
+                'home.html',
+                {'new_item_text': '신규 작업 아이템'},request=request
+        )
+        self.assertEqual(response.content.decode(), expected_html)
+
+
+
+
+
+
+
+
+
+
+
 
