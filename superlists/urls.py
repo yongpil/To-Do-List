9a1c0c8c03383513a6f3429506a976f2da5652db
yongpil <myp7166@naver.com>
2016-08-26 16:27:16 +0900

    두 개 폼을 위한 URL 적용, views와 tests에 있는 코드 이동, 신규 URL

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
-from lists.views import home_page,view_list
+from lists.views import home_page,view_list,new_list
 
 urlpatterns = [
     url(r'^$',home_page,name='home'),
     url(r'^lists/the-only-list-in-the-world/$',view_list,
         name='view_list'
     ),
+    url(r'^lists/new$',new_list,name='new_list'),
 #    url(r'^admin/', admin.site.urls),
 ]
