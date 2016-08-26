765d6e9f7018034f7780a92fb316b5be94302796
yongpil <myp7166@naver.com>
2016-08-26 15:32:05 +0900

    목록 출력을 위한 신규 URL,뷰,템플릿

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+        
     def test_can_start_a_list_and_retrieve_it_later(self):    
         # 에디스(Edith)는 멋진 작업 목록 온란인 앱이 나왔다는 소식을 듣고
         # 해당 웹 사이트를 확인하러 간다
@@ -19,7 +24,7 @@ class NewVisitorTest(LiveServerTestCase):
         # 웹 페이지 타이틀과 헤더가 'To-Do'를 표시하고 있다
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do',header_text)
+        self.assertIn('작업 목록 시작',header_text)
 
         # 그녀는 바로 작업을 추가하기로 한다
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -37,7 +42,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url 
         self.assertRegex(edith_list_url, '/lists/.+')
-        self.check_for_row_in_list_table('1:공작깃털 사기')
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다 (에디스는 매우
@@ -76,7 +81,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # 프란시스가 새로운 작업 아이템을 입력하기 시작한다
         # 그는 에디스보다 재미가 없다
-        inputbox = self.brower.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('우유 사기')
         inputbox.send_keys(Keys.ENTER)
 
