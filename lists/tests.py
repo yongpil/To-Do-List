a2a0c1f2144bbe06a18a3354233b52979b09e8e2
yongpil <myp7166@naver.com>
2016-08-26 17:34:20 +0900

    List모델 생성 및 Item모델과 연계

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page 
-from lists.models import Item
+from lists.models import Item,List
 
 class HomePageTest(TestCase):
 
@@ -18,14 +18,20 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html',request=request)
         self.assertEqual(response.content.decode(), expected_html)
 
-class ItemModelTest(TestCase):
-    def test_saving_and_retrieving_tiems(self):
+class ListAndItemModelsTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = '첫 번째 아이템'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = '두 번째 아이템'
+        second_item.list = list_
         second_item.save()
 
         saved_items = Item.objects.all()
@@ -34,7 +40,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, '첫 번째 아이템')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
+        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
@@ -43,8 +51,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response,'list.html')
 
     def test_display_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
