cf6204dbf142d6d5aeac789b42c78594ef0517ef
yongpil <myp7166@naver.com>
2016-08-28 18:11:09 +0900

    기존 리스트에 아이템을 추가하기 위한 신규 URL 및 뷰. FT 성공^^)

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
-from lists.views import home_page,view_list,new_list
+from lists.views import home_page,view_list,new_list,add_item
 
 urlpatterns = [
     url(r'^$',home_page,name='home'),
-    url(r'^lists/the-only-list-in-the-world/$',view_list,
+    url(r'^lists/(\d+)/$',view_list,
         name='view_list'
     ),
     url(r'^lists/new$',new_list,name='new_list'),
+    url(r'^lists/(\d+)/add_item$',add_item,name='add_item'),
 #    url(r'^admin/', admin.site.urls),
 ]
