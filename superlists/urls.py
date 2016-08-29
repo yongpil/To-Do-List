6d65019e73a890db9d058cf291ab006ab7930d91
yongpil <myp7166@naver.com>
2016-08-29 11:18:13 +0900

    레이아웃과 스타일링을 위한 FT 첫 단계

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,13 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
-from lists.views import home_page,view_list,new_list,add_item
+from lists.views import home_page
 
 urlpatterns = [
     url(r'^$',home_page,name='home'),
-    url(r'^lists/(\d+)/$',view_list,
-        name='view_list'
-    ),
-    url(r'^lists/new$',new_list,name='new_list'),
-    url(r'^lists/(\d+)/add_item$',add_item,name='add_item'),
+    url(r'^lists/', include('lists.urls')),
 #    url(r'^admin/', admin.site.urls),
 ]
