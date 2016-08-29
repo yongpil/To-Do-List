e7ea3288dfad2f88d5ef33fc17800d873cc3b660
yongpil <myp7166@naver.com>
2016-08-29 15:14:10 +0900

    레이아웃 개선을 위한 부트스트랩 사용

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
 # _*_ coding: utf-8 _*_
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
         
     def setUp(self):
         self.browser = webdriver.Firefox()
--- lists/tests.py
+++ lists/tests.py
@@ -49,7 +49,7 @@ class ListViewTest(TestCase):
     def test_passes_correct_list_to_template(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
-        response = self.client.get('/lists/%d/' % (correct_list,id))
+        response = self.client.get('/lists/%d/' % (correct_list.id))
         self.assertEqual(response.context['list'],correct_list)
 
     def test_uses_list_template(self):
