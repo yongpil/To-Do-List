32f442d64dbd3a31a9b6737b6d65bc78b16f4582
yongpil <myp7166@naver.com>
2016-09-02 16:20:27 +0900

    모델 검증을 위한 신규 목록 뷰 수정

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from lists.models import Item,List
-
+from django.core.exceptions import ValidationError
 class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -26,3 +26,10 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, '두 번째 아이템')
         self.assertEqual(second_saved_item.list, list_)
+    
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_,text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.utils.html import escape
 
 from lists.views import home_page 
 from lists.models import Item,List
@@ -46,6 +47,7 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, '다른 목록 아이템 1')
         self.assertNotContains(response, '다른 목록 아이템 2')
+
 class NewListTest(TestCase):
 
     def test_can_save_a_POST_request_to_an_existing_list(self):
@@ -89,23 +91,14 @@ class NewListTest(TestCase):
         new_list = List.objects.first()
         self.assertRedirects(response,'/lists/%d/' % (new_list.id,))
 
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text':''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
 
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def test_invalid_list_items_arent_saved(self):
+        self.client.post('/lists/new', data={'item_text':''})
+        self.assertEqual(List.objects.count(),0)
+        self.assertEqual(Item.objects.count(),0)
